68199c1a613f76f59681a8bc961d3328c3ca6d22
Whitney Carter <whitneycarter@SCWDBs-MacBook-Pro-3.local>
2021-02-23 11:22:44 -0600

    Completed Exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,55 @@
+// JS Basics Exercise
+// 1
+console.log(14);
+
+// 2
+// Numbers, Undefined, Strings, Booleans, Null
+
+// 3
+// Addition is +, Subtraction is -, Multiplication *, Division is /, Exponentiation is **, Modulus is %
+
+// 4
+console.log(5%2);
+console.log(5%4);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6
+console.log("Wanda" + "Vision");
+
+// 7
+console.log("Matt".length + "Riley".length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log("I".length % "Am".length - "Iron".length % "Man".length);
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+console.log("I".length + "Am".length + ("Iron".length % 2) - "Man".length);
+
+// 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("HAWKEYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// console.log("CAP"[1] + "\n" + "VISION"[0] + "\n" + "HAWKEYE"[4] + "\n" + "NICK FURY"[0] + "\n" + "GROOT"[0] + "\n" + "DOCTOR STRANGE"[13] + "\n" + "THOR"[3] + "\n" + "STAR-LORD"[0]);
+
+// 5
+var m = 10;
+var c = 5;
+var u = m + c;
+console.log(u);
